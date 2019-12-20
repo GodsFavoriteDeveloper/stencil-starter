44c2c7a8b5f244693408a23a85085f45f1d0d36a
David Velasco <davvelsan@gmail.com>
2019-12-20 18:10:30 +0100

    tailwindcss ref, style

--- src/components/app-home/app-home.tsx
+++ src/components/app-home/app-home.tsx
@@ -11,10 +11,18 @@ export class AppHome {
     return (
       <div class='app-home'>
         <p>
-          Welcome to the Stencil App Starter.
+          Welcome to the Stencil + TailwindCSS App Starter.
+        </p>
+
+        <p>
           You can use this starter to build entire apps all with
-          web components using Stencil!
-          Check out our docs on <a href='https://stenciljs.com'>stenciljs.com</a> to get started.
+          web components using Stencil and TailwindCSS!
+        </p>
+
+        <p>
+          Check out the docs on
+          <a href='https://stenciljs.com'> stenciljs.com</a> and
+          <a href="https://tailwindcss.com/"> tailwindcss.com</a> to get started.
         </p>
 
         <stencil-route-link url='/profile/stencil'>
