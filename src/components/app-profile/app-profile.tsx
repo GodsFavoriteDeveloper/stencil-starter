94712627c9473febf37941d01b4585915a7cb7e1
David Velasco <davvelsan@gmail.com>
2019-12-20 15:39:04 +0100

    private internal class method

--- src/components/app-profile/app-profile.tsx
+++ src/components/app-profile/app-profile.tsx
@@ -9,7 +9,7 @@ import { MatchResults } from '@stencil/router';
 export class AppProfile {
   @Prop() match: MatchResults;
 
-  normalize(name: string): string {
+  private normalize(name: string): string {
     if (name) {
       return name.substr(0, 1).toUpperCase() + name.substr(1).toLowerCase();
     }
